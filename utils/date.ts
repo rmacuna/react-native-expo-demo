2ab519b80123d39bdbf56e34d6696596336ec80e
rmacuna <rmacuna@uninorte.edu.co>
2019-07-02 08:37:24 -0500

    Aádido typescript al proyecto

--- utils/date.ts
+++ utils/date.ts
@@ -1,5 +1,3 @@
-
-
 export const formatToReadableDate = (stringDate) => {
 // El objeto recibido se espera que sea de tipo [Day Month XX YEAR HOURS:MINUTS:SECONDS GMT-0500 (-05)]
     let humanDate = stringDate.toString().split(' ').slice(0, 4).join(' ');
@@ -10,3 +8,4 @@ export const formatToReadableTime = (stringTime) => {
     let timeDate = stringTime.toString().split(' ').slice(3, 1)
     return timeDate
 }
+
